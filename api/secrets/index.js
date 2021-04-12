1e4408f60e5bbf48d2acbd41a02f93a5676ae7da
jon del toro <61124054+jdeltoro88@users.noreply.github.com>
2021-04-11 21:45:37 -0600

    update

--- api/secrets/index.js
+++ api/secrets/index.js
@@ -5,7 +5,10 @@
 
   If no fallback is provided, TESTS WON'T WORK and other
   developers cloning this repo won't be able to run the project as is.
- */
+*/
+
+  const JWT_SECRET = process.env.JWT_SECRET || "shh" 
+ 
 module.exports = {
 
 }
